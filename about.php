5d78742f89e57eeb9dfa768aa756982e4bef689f
DESKTOP-TINM1CF <abananyabera@gmail.com>
2022-06-04 16:29:54 +0530

    initial commit

---
+++ about.php
@@ -0,0 +1,92 @@
+<?php
+    include "includes/header.php";
+?>
+		<div class="col-md-9 main" >
+		<!--banner-section-->
+            <div class="banner-section">
+            <!--/top-news-->
+            <div class="top-news">
+                <div class="about-content">
+                    <!-- about -->
+                    <h3 class="tittle">About US <i class="glyphicon glyphicon-user"></i></h3>
+                    <img src="image_uploads/CollegeGate.png" alt="" />
+                    <p>The college was established in 1998 with the assistance of West Bengal Power Development Corporation Limited (WBPDCL), an enterprise of Govt. of West Bengal.
+
+An educational institute with proven track record of excellence, the college is spread over 32 acres of land and located within integrated campus of Kolaghat Thermal Power Plant Township of WBPDCL. The college is aesthetically landscaped & provides a serene and peaceful atmosphere.
+
+Only engineering college in the country situated within integrated campus of Govt. Thermal Power Plant.
+
+Good governance under MIC, Power, Govt. of West Bengal as Chairman of the College.
+
+
+                </div>
+                <!--div class="top-inner second">
+                    <div class="col-md-6 top-text">
+                        <a href="single.html"><img src="images/pic1.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 25 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="col-md-6 top-text two">
+                        <a href="single.html"><img src="images/pic2.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 27 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div-->
+            </div>
+            <!--//top-news-->
+        </div>
+    <!--//banner-section-->
+    <div class="banner-right-text">
+        <h3 class="tittle">Our Team <i class="glyphicon glyphicon-star"></i></h3>
+        <!--/general-news-->
+        <div class="general-news">
+            <div class="general-inner">
+                <div class="edit-pics">
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f4.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a>SOUVIK SARKAR</a></h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a>ANANYA BERA</a></h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a>SHUVENDU DAS</a></h5>
+                            <div class="td-post-date two"> </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--//general-news-->
+        <!--/news-->
+        <!--/news-->
+    </div>
+    </div>
+    <div class="clearfix"> </div>
+<?php
+include "includes/footer.php";
+?>
---
+++ includes/footer.php
@@ -0,0 +1,49 @@
+<!--/footer-->
+<div class="footer">
+    <div class="footer-top">
+        <div class="col-md-4 footer-grid">
+            <h4>CEMK PLACEMENT CELL</h4>
+            <ul class="bottom">
+                <li>Designed By: souvik,ananya,shuvendu</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Message Us Now</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-home"></i>Available 24/7 </li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="cse1812@cemk.ac.in">cse1812@cemk.ac.in</a></li>
+                <li><i class="glyphicon glyphicon-envelope"></i>9876543210</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Address Location</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-map-marker"></i>Kolaghat Thermal Power Plant Township, Kolaghat, West Bengal 721171.</li>
+                <li><i class="glyphicon glyphicon-earphone"></i>Mobile: 96090 64108 </li>
+            </ul>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="copy">
+    <p>&copy; 2022 CEMK PLACEMENT CELL All Rights Reserved</p>
+</div>
+<div class="clearfix"> </div>
+<!--//footer-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    });
+</script>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
